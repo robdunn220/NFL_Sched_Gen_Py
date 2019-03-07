30e4abc54bfdec157d90698d9bdc479198d0a54a
Robert Dunn <robertdunn220@gmail.com>
2019-03-07 13:40:22 -0500

    Succesfully added in-conference, out-of-division matchups

--- diff_sched_gen.py
+++ diff_sched_gen.py
@@ -4,8 +4,8 @@ import random
 NFL = new_league_gen.league
 
 def divSchedGen():
-    for conf, div in NFL.items():
-        for div, teams in div.items():
+    for conf, divs in NFL.items():
+        for div, teams in divs.items():
             for team in teams:
                 for opp in teams:
                     if opp.name not in team.schedule['Sched'] and team.name != opp.name:
@@ -18,9 +18,31 @@ def divSchedGen():
                         opp.schedule['Sched'][x] = team.name
 
 def inConfGen():
-    pass
+    for conf, divs in NFL.items():
+        if conf == 'NFC':
+            divList = list(divs.keys())
+            divMatchups = []
+            for i in range(0,2):
+                x = random.choice(divList)
+                divList.remove(x)
+                divMatchups.append(x)
+            divMatchupsOne = [[], []]
+            divMatchupsTwo = [[], []]
+            for n in range(0,2):
+                divMatchupsOne[n].append(NFL['NFC'][divList[n]])
+                divMatchupsTwo[n].append(NFL['NFC'][divMatchups[n]])
+            for m in range(0,1):
+                for t in range(0,4):
+                    for teams in divMatchupsOne[t]:
+                        print(teams)
+                        for opponents in divMatchupsTwo[t]:
+                            for team in range(0,4):
+                                x = random.choice(teams[team].schedule['Weeks'])
+                                while x not in opponents[team].schedule['Weeks']:
+                                    x = random.choice(teams[team].schedule['Weeks'])
+                                teams[team].schedule['Weeks'].remove(x)
+                                teams[team].schedule['Sched'][x] = opponents[team].name
+                                opponents[team].schedule['Weeks'].remove(x)
+                                opponents[team].schedule['Sched'][x] = teams[team].name
 
-
-for conference, divisions in NFL.items():
-    x = random.choice(list(divisions.keys()))
-    print(x)
+inConfGen()
--- sched_gen_oop.py
+++ sched_gen_oop.py
@@ -18,22 +18,44 @@ def divSched():
                 opponent.schedule['Sched'][x] = team.name
 
 # Function for randomly assigning inter-divisional opponents for each team. Don't have random assignment yet.
-def inConfSched(confState = 0):
-    divs = ['North', 'South', 'East', 'West']
-    divMatcher = []
-    for i in range(0, 2):
-        div = random.choice(divs)
-        divMatcher.append(div)
-        divs.remove(div)
+def inConfSched():
+    for q in range(0,2):
+        divMatchOne = ['North', 'South', 'East', 'West']
+        divMatchTwo = []
+        for i in range(0, 2):
+            div = random.choice(divMatchOne)
+            divMatchTwo.append(div)
+            divMatchOne.remove(div)
 
-    if confState == 0:
-        for team in NFL:
-            if team.conf == 'NFC':
-                
+        for z in range(0,2):
+            for team in NFL:
+                if q == 0:
+                    if team.conf == 'NFC' and team.div == divMatchOne[z]:
+                        for opponent in NFL:
+                            if opponent.conf == 'NFC' and opponent.div == divMatchTwo[z]:
+                                x = random.choice(team.schedule['Weeks'])
+                                while x not in opponent.schedule['Weeks']:
+                                    x = random.choice(team.schedule['Weeks'])
+                                team.schedule['Weeks'].remove(x)
+                                team.schedule['Sched'][x] = opponent.name
+                                opponent.schedule['Weeks'].remove(x)
+                                opponent.schedule['Sched'][x] = team.name
+                if q == 1:
+                    if team.conf == 'AFC' and team.div == divMatchOne[z]:
+                        for opponent in NFL:
+                            if opponent.conf == 'AFC' and opponent.div == divMatchTwo[z]:
+                                x = random.choice(team.schedule['Weeks'])
+                                while x not in opponent.schedule['Weeks']:
+                                    x = random.choice(team.schedule['Weeks'])
+                                team.schedule['Weeks'].remove(x)
+                                team.schedule['Sched'][x] = opponent.name
+                                opponent.schedule['Weeks'].remove(x)
+                                opponent.schedule['Sched'][x] = team.name
 
-# divSched()
+divSched()
 inConfSched()
-# for team in NFL:
-#     print('Team:', team.name)
-#     for key in sorted(team.schedule['Sched']):
-#         print('%s: %s' % (key, team.schedule['Sched'][key]))
+for team in NFL:
+    if team.conf == 'AFC' and team.div == 'South':
+        print('Team:', team.name)
+        for key in sorted(team.schedule['Sched']):
+            print('%s: %s' % (key, team.schedule['Sched'][key]))
