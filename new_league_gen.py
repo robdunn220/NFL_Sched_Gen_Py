3cc1f7ee469b7a65bd8bb6446daa43409774caf7
robdunn220 <robertdunn220@gmail.com>
2019-04-29 13:35:57 -0400

    Able to correctly assign all opponents to schedule. Unable to assign weeks properly

--- new_league_gen.py
+++ new_league_gen.py
@@ -3,33 +3,64 @@ import random
 
 # Assign imported list of Team objects
 NFL = league_gen.Team._registry
-random.shuffle(NFL)
+NFL = random.sample(NFL, len(NFL))
 
-def scheduler(team, opponent):
-    x = random.choice(team.schedule['Weeks'])
-    while x not in opponent.schedule['Weeks']:
-        x = random.choice(team.schedule['Weeks'])
-    team.schedule['Weeks'].remove(x)
-    team.schedule['Sched'][x] = opponent
-    opponent.schedule['Weeks'].remove(x)
-    opponent.schedule['Sched'][x] = team
+def scheduler(team, opponent, week):
+    weekMatch = sorted(set(team.weeks) & set(opponent.weeks))
+    if len(weekMatch) > 0:
+        x = random.choice(weekMatch)
+        if x:
+            team.weeks.remove(x)
+            opponent.weeks.remove(x)
+            team.schedule.append({x: {opponent.name: opponent}})
+            opponent.schedule.append({x: {team.name: team}})
+
+def repeatSchedDebug(team, opponent, week):
+    listOfOpp = []
+    # Creates a list of opponent names in team schedule
+    for x in team.schedule:
+        for week, matchup in x.items():
+            for oppname, opp in matchup.items():
+                listOfOpp.append(oppname)
+    # Uses created list to check if opponent is already on schedule to stop double-scheduling issue
+    if opponent.name not in listOfOpp:
+        scheduler(team, opponent, week)
 
 def divSchedGen():
-    for team in NFL:
-        teamList = []
-        for opponent in NFL:
-            # Checks if opponent is in-conference
-            if team.name == 'Falcons' and team.conf == opponent.conf and team.name != opponent.name:
-                # Checks if team is in-division
-                if team.div == opponent.div:
-                    scheduler(team, opponent)
-                elif opponent.name == 'Saints':
-                    scheduler(team, opponent)
+    for team_one in NFL:
+        match = False
+        week = 1
+        while match == False:
+            for team_two in NFL:
+                if team_one.name != team_two.name:
+                    if team_one.conf == team_two.conf:
+                        # Checks if team_two is in-division
+                        if team_one.div == team_two.div:
+                            match = True
+                            week += 1
+                            scheduler(team_one, team_two, week)
+                        # Checks if team_two is out-of-division, in-conference, and has been assigned as divisional team_two
+                        elif team_one.div == team_two.div_match:
+                            match = True
+                            week += 1
+                            repeatSchedDebug(team_one, team_two, week)
+                        # Checks if team_two is out-of-division, in-conference, and has the same previous years division ranking
+                        elif team_one.div != team_two.div_match and team_one.previous_div_rank == team_two.previous_div_rank:
+                            match = True
+                            week += 1
+                            repeatSchedDebug(team_one, team_two, week)
+                    elif team_one.conf != team_two.conf:
+                        if team_one.div == team_two.div:
+                            match = True
+                            week += 1
+                            repeatSchedDebug(team_one, team_two, week)
 
 divSchedGen()
 
 for team in NFL:
-    for opponent in NFL:
-        if team.name == 'Falcons':
-            for t in list(team.schedule['Sched'].items()):
-                print(t[1].name)
+    if team.name == 'Packers':
+        print(len(team.schedule))
+        for x in team.schedule:
+            for week, matchup in x.items():
+                for oppname, opp in matchup.items():
+                    y = 1
